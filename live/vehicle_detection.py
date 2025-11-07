10271584957334cdc7edb413f34f8e161b11f891
Nipunikumudika <nipunikumudika@gmail.com>
2025-11-07 18:29:01 +0530

    changing name helmet triple

--- live/vehicle_detection.py
+++ live/vehicle_detection.py
@@ -1,7 +1,7 @@
 import cv2
 from ultralytics import YOLO
 from speed_detection import calculate_speed
-from helmet_detection import check_helmet_triple
+from live.helmet_triple_detection import check_helmet_triple
 
 # ------------------- Main Detection ------------------- #
 def detect_vehicles(video_source, calibration):
