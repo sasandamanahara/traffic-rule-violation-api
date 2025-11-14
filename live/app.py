127b14319fa8436087a78ebea0bab1eaccc09e75
Nipunikumudika <nipunikumudika@gmail.com>
2025-11-14 14:05:51 +0530

    adding isDirection and get direction

--- live/app.py
+++ live/app.py
@@ -1,12 +1,18 @@
 from initialize import initialize_stream
+from check_direction_initialize import track_vehicle_line_order
 from vehicle_detection import detect_vehicles
 
 if __name__ == "__main__":
-    video_source = "1.mp4"  # or RTSP URL
+    video_source = "2.mp4"  # or RTSP URL
 
     print("[APP] Starting initialization...")
     calibration = initialize_stream(video_source)
 
+    vehicle_data = track_vehicle_line_order(video_source, calibration)
+    print("[APP] Vehicle line crossing data collected.")
+    print(vehicle_data['isDirection'])
+    print(vehicle_data['direction'])
+  
     if calibration:
         print("[APP] Calibration complete. Starting detection system...")
         detect_vehicles(video_source, calibration)
